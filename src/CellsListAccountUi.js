d513ae4ee26184f8a5bf0a33228c84e68f06eba9
Yojan Pinto <ypinto.vela@gmail.com>
2022-07-12 01:14:02 -0500

    listaccount ui - test

--- src/CellsListAccountUi.js
+++ src/CellsListAccountUi.js
@@ -1,6 +1,8 @@
-import { LitElement, html, } from 'lit-element';
+import { LitElement, html } from 'lit-element';
 import { getComponentSharedStyles } from '@bbva-web-components/bbva-core-lit-helpers';
 import styles from './CellsListAccountUi-styles.js';
+import '@bbva-web-components/bbva-web-card-product/bbva-web-card-product.js';
+import '@bbva-web-components/bbva-web-button-default/bbva-web-button-default.js';
 /**
 ![LitElement component](https://img.shields.io/badge/litElement-component-blue.svg)
 
@@ -24,28 +26,92 @@ export class CellsListAccountUi extends LitElement {
   // Declare properties
   static get properties() {
     return {
-      name: { type: String, },
+      data: { type: Object },
     };
   }
 
   // Initialize properties
   constructor() {
     super();
-    this.name = 'Cells';
+    this.data = [];
   }
 
   static get styles() {
     return [
       styles,
-      getComponentSharedStyles('cells-list-account-ui-shared-styles')
+      getComponentSharedStyles('cells-list-account-ui-shared-styles'),
     ];
+  }
+
+  _fire(nameEvent, detail) {
+    const val = typeof detail === 'undefined' ? null : detail;
+    this.dispatchEvent(
+      new CustomEvent(nameEvent, {
+        composed: true,
+        bubbles: true,
+        detail: val,
+      })
+    );
+  }
+
+  _onViewDetail(account) {
+    const payload = { id: account.id };
+    this._fire('view-detail-account', payload);
+  }
+
+  _formatAmount(currency, amount) {
+    switch (currency) {
+      case 'PEN':
+        return `S/ ${Number(amount).toFixed(2)}`;
+      case 'USD':
+        return `$ ${Number(amount).toFixed(2)}`;
+      default:
+        return '';
+    }
   }
 
   // Define a template
   render() {
     return html`
-      <slot></slot>
-      <p>Welcome to ${this.name}</p>
+      <div class="container">
+        <div class="accounts">
+          <div class="accounts__header">Mis Cuentas</div>
+
+          ${
+            this.data.length === 0
+              ? html`<div class="accounts__list--empty">
+                  No tienes cuentas.
+                </div>`
+              : html`<div class="accounts__list">
+                  <div class="accounts__list-item accounts-list-item">
+                    ${this.data.map(
+                      (account) => html`<div class="accounts-list-item__detail">
+                        <bbva-web-card-product
+                          subheading=${account.productName}
+                          button-text="Ver detalle"
+                          @button-click=${() => this._onViewDetail(account)}
+                        >
+                          <bbva-web-list-item-bullet slot="option">
+                            Numero de cuenta: <br />
+                            ${account.number}
+                          </bbva-web-list-item-bullet>
+
+                          <bbva-web-list-item-bullet slot="option">
+                            Saldo disponible: <br />
+                            ${this._formatAmount(
+                              account.currency,
+                              account.amoutAvailable
+                            )}
+                          </bbva-web-list-item-bullet>
+                        </bbva-web-card-product>
+                      </div>`
+                    )}
+                  </div>
+                </div>`
+          }
+          </div>
+        </div>
+      </div>
     `;
   }
 }
--- test/cells-list-account-ui.test.js
+++ test/cells-list-account-ui.test.js
@@ -1,20 +1,75 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { html, fixture, assert, fixtureCleanup } from '@open-wc/testing';
 import '../cells-list-account-ui.js';
+import sinon from 'sinon';
 
 suite('CellsListAccountUi', () => {
   let el;
 
   teardown(() => fixtureCleanup());
 
+  let data = [
+    {
+      id: 'U0dJQ3NneXRRNlZCWTVuZnpHS3E',
+      number: '00110241000299979661',
+      currency: 'PEN',
+      productName: 'CUENTA FACIL',
+      amoutAvailable: 2834.14,
+    },
+
+    {
+      id: 'b1dVSldRdDhXa3luTnFVSWdxTjg',
+      number: '00110814000200089516',
+      currency: 'PEN',
+      productName: 'CUENTA INDEPENDENCIA',
+      amoutAvailable: 0.0,
+    },
+  ];
+
   setup(async () => {
-    el = await fixture(html`<cells-list-account-ui></cells-list-account-ui>`);
+    el = await fixture(
+      html`<cells-list-account-ui .data=${data}></cells-list-account-ui>`
+    );
     await el.updateComplete;
   });
 
-  test('instantiating the element with default properties works', () => {
-    const element = el.shadowRoot.querySelector('p');
-    assert.equal(element.innerText, 'Welcome to Cells');
+  test('Method _formatAmount', async () => {
+    el._formatAmount('PEN', 100);
   });
 
+  test('Method _formatAmount', async () => {
+    el._formatAmount('USD', 100);
+  });
+
+  test('Method _formatAmount', async () => {
+    el._formatAmount('', 0);
+  });
+
+  test('Method _onViewDetail', async () => {
+    const spy = sinon.spy();
+    el.addEventListener('view-detail-account', spy);
+    el._onViewDetail({ id: 'b1dVSldRdDhXa3luTnFVSWdxTjg' });
+    assert.isTrue(spy.called);
+  });
+
+  test('onClick _onViewDetail', async () => {
+    let elementCard = el.shadowRoot.querySelector('bbva-web-card-product');
+    const buttonCard = elementCard.shadowRoot.querySelector(
+      'bbva-web-button-default'
+    );
+    buttonCard.click();
+  });
+
+  test('Method _fire', async () => {
+    const payload = { id: 'b1dVSldRdDhXa3luTnFVSWdxTjg' };
+    el._fire('view-detail-account', payload);
+  });
+
+  test('Method _fire', async () => {
+    el._fire('view-detail-account', undefined);
+  });
+
+  test('change  data', async () => {
+    el.data = [];
+  });
 });
